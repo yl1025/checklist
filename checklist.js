651947b120f88abc0d2c061abd5b8abc0bda4f5c
yl1025 <yl1025@scarletmail.rutgers.edu>
2021-07-20 21:10:58 -0400

    checklist
    updated

--- checklist.js
+++ checklist.js
@@ -2,8 +2,23 @@ function addButtonPress() {
   var itemInput = document.getElementById("item-input").value;
   var li = document.createElement("li");
 
-  var t = document.createTextNode(itemInput);
+  let badwords = /fuck|ass|cunt|shit|nignog/gi;
+  let nobadwords = itemInput;
+  var asterik = "";
+  for (var i = 0; i < itemInput.length; i++) {
+    asterik += "*";
+  }
+
+  let replacebadwords = nobadwords.replace(badwords, asterik);
+  document.getElementById("item-input").value = replacebadwords;
+
+  var t = document.createTextNode(replacebadwords);
   li.appendChild(t);
+
+  if (itemInput === "") {
+    return;
+  }
+
   document.getElementById("list").appendChild(li);
   document.getElementById("item-input").value = "";
 
@@ -27,3 +42,16 @@ enter.addEventListener("keyup", function (event) {
     document.getElementById("myBtn").click();
   }
 });
+
+/*  let badwords = /fuck|ass|cunt|nignog/gi;
+  let nobadwords = itemInput;
+  let replacebadwords = nobadwords.replace(badwords, "******");
+  document.getElementById("item-input").value = replacebadwords
+for (var i=0; i < itemInput.length; i++) ;
+  */
+
+//issue1: press enter and do nothing
+//issue2: uncheck/uncross an item in case u thought u finished it
+//issue3: censor bad words, keep the first letter but rest is f***
+//issue4: CSS stuff, layout, font, etc.
+//issue5:
